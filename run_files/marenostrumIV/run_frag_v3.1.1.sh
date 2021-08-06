ebba4f5b30cd57034435be1a61262d38e20840cc
Carles Perez Lopez <carlesperez94@gmail.com>
2021-08-06 15:32:54 +0200

    Update run_frag_v3.1.1.sh

--- run_files/marenostrumIV/run_frag_v3.1.1.sh
+++ run_files/marenostrumIV/run_frag_v3.1.1.sh
@@ -18,3 +18,5 @@ conda activate /gpfs/projects/bsc72/conda_envs/platform/1.6.0
 
 unset PYTHONPATH
 export PYTHONPATH="/gpfs/projects/bsc72/LibPrep/v1.1.1/lib_prep:/gpfs/projects/bsc72/FragPELE/v3.1.1-beta/frag_pele"
+
+python -m frag_pele.main -sef serie_file.conf -cp 1w7h_preparation_structure_no_w.pdb --cpus 4 -x 2 -es 10 -rot 10 -ff OPLS2005
